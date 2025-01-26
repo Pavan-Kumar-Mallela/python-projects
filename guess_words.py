3b4caf1d1a1e47e3b7a8120ff9b4642f7600110b
Pavan-Kumar-Mallela <167163832+Pavan-Kumar-Mallela@users.noreply.github.com>
2025-01-26 11:43:06 +0530

    Create guess_words.py

---
+++ guess_words.py
@@ -0,0 +1,129 @@
+import random
+HANGMAN = (
+    """
+ ------
+ |    |
+ |
+ |
+ |
+ |
+ |
+ |
+ |
+----------
+""",
+"""
+ ------
+ |    |
+ |    O
+ |
+ |
+ |
+ |
+ |
+ |
+----------
+""",
+"""
+ ------
+ |    |
+ |    O
+ |   -+-
+ | 
+ |   
+ |   
+ |   
+ |   
+----------
+""",
+"""
+ ------
+ |    |
+ |    O
+ |  /-+-
+ |   
+ |   
+ |   
+ |   
+ |   
+----------
+""",
+"""
+ ------
+ |    |
+ |    O
+ |  /-+-/
+ |   
+ |   
+ |   
+ |   
+ |   
+----------
+""",
+"""
+ ------
+ |    |
+ |    O
+ |  /-+-/
+ |    |
+ |   
+ |   
+ |   
+ |   
+----------
+""",
+"""
+ ------
+ |    |
+ |    O
+ |  /-+-/
+ |    |
+ |    |
+ |   | 
+ |   | 
+ |   
+----------
+""",
+"""
+ ------
+ |    |
+ |    O
+ |  /-+-/
+ |    |
+ |    |
+ |   | |
+ |   | |
+ |  
+----------
+""")
+
+
+attempts= len(HANGMAN) - 1
+wordList = ["happy", "tamilnadu", "cat", "facebook", "random"]
+
+word = random.choice(wordList)
+
+guessedWord = ['_'] * len(word)
+
+
+while attempts > 0:
+   
+    print("\nCurrent word: " + ' '.join(guessedWord))
+
+    guess = input("Guess a letter: ").lower()
+   
+    if guess in word:
+        for i in range(len(word)):
+            if word[i] == guess:
+                guessedWord[i] = guess
+        print("Great guess!")
+    else:
+        attempts -= 1
+        print("Wrong guess! Attempts left: " + str(attempts)
+        print(HANGMAN[len(HANGMAN)-attempts)])
+      
+    if '_' not in guessedWord:
+        print("\nCongratulations!! You guessed the word: " + word)
+        break
+else:
+    print("\nYou've run out of attempts! The word was: " + word)
